0f99f9c5ef1aa931ceb0a4d507fe99357c01c008
Yoann Cribier <ycribier@student.42.fr>
2014-11-16 18:09:21 +0100

    more classes

---
+++ Assets/YCR/Scripts/ActionController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionController : MonoBehaviour {
+
+    public GameObject target;
+
+    private PhotonView photonView;
+
+	// Use this for initialization
+	void Start () {
+        photonView = GetComponent<PhotonView>();
+        Debug.Log(PhotonNetwork.player);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnMouseUp () {
+        if (!photonView.isMine)
+        {
+            target = this.gameObject;
+        }
+    }
+}
---
+++ Assets/YCR/Scripts/KeyController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KeyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+        keyHandler();
+	}
+
+    void keyHandler () {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            //atk
+            Debug.Log("ATK");
+        }
+    }
+}
